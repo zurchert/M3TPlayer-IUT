8f86f1d67245d698a007825e32af2160a778df3d
TZK <thib-17@orange.fr>
2015-05-09 19:01:27 +0200

    Adding to M3TPlayer class a default constructor

--- M3TPlayer/src/fr/iutvalence/m3tplayer/M3TPlayer.java
+++ M3TPlayer/src/fr/iutvalence/m3tplayer/M3TPlayer.java
@@ -2,4 +2,24 @@ package fr.iutvalence.m3tplayer;
 
 public class M3TPlayer {
 
+	/**
+	 * The volume of the player.
+	 * Value is between 0 and 100.
+	 */
+	private int volume;
+	
+	/**
+	 * Random playing mod
+	 */
+	private boolean randomPlaying;
+
+	/**
+	 * Initializes the player with default values.
+	 * Sets the volume to 100 percent, and the random mode to false.
+	 */
+	public M3TPlayer() {
+		this.volume = 100;
+		this.randomPlaying = false;
+	}
+	
 }
