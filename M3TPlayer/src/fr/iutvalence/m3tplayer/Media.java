1b291a9a5605966f8962027eae05c5dd9cbb0335
TZK <thib-17@orange.fr>
2015-05-09 19:03:39 +0200

    Adding to Media class a constructor with parameters

--- M3TPlayer/src/fr/iutvalence/m3tplayer/Media.java
+++ M3TPlayer/src/fr/iutvalence/m3tplayer/Media.java
@@ -2,4 +2,25 @@ package fr.iutvalence.m3tplayer;
 
 public class Media {
 
+	/**
+	 * The media's id
+	 */
+	protected int id;
+	
+	/**
+	 * The media's name
+	 */
+	protected String title;
+
+	
+	/**
+	 * Creates a new Media with given data
+	 * @param id The media's id
+	 * @param title The media's name
+	 */
+	public Media(int id, String title) {
+		this.id = id;
+		this.title = title;
+	}
+	
 }
