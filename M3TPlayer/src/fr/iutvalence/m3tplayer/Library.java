a320b751af7f6f9b98ea407b80b0cc200f272181
TZK <thib-17@orange.fr>
2015-05-09 20:45:16 +0200

    Adding attributes and completing constructor

--- M3TPlayer/src/fr/iutvalence/m3tplayer/Library.java
+++ M3TPlayer/src/fr/iutvalence/m3tplayer/Library.java
@@ -6,17 +6,42 @@ import java.util.Map;
 public class Library {
 	
 	/**
-	 * The list of the imported medias.
-	 * Medias are linked with their unique ID
+	 * The list of the imported music.
+	 * Music are linked with their unique ID
 	 */
-	private Map<Integer, Media> listMedia;
+	private Map<Integer, Radio> listRadios;
 
+	/**
+	 * The list of the imported music.
+	 * Music are linked with their unique ID
+	 */
+	protected Map<Integer, Music> listMusics;
+	
+	/**
+	 * The number of music which were imported into the library
+	 */
+	protected int importedMusicNumber;
+	
+	/**
+	 * The sum of the lenght of all musics imported into the library.
+	 */
+	protected float totalMusicLenght;
+	
 	/**
 	 * Initializes the library.
 	 * At this moment, the library is empty.
 	 */
 	public Library() {
-		this.listMedia = new HashMap<Integer, Media>();
+		this.listRadios = new HashMap<Integer, Radio>();
+		this.listMusics = new HashMap<Integer, Music>();
+		this.totalMusicLenght = 0;
+		this.importedMusicNumber = 0;
 	} 
 	
+	/**
+	 * Import a media into the library.
+	 */
+	public void importMedia(){
+		// TODO complete
+	}
 }
