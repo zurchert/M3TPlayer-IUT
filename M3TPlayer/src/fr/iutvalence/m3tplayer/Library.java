83fc47c9344b9ff6c9399ef601f53d62569c4007
TZK <thib-17@orange.fr>
2015-05-09 20:31:54 +0200

    Adding constructor to the Library class

--- M3TPlayer/src/fr/iutvalence/m3tplayer/Library.java
+++ M3TPlayer/src/fr/iutvalence/m3tplayer/Library.java
@@ -1,5 +1,22 @@
 package fr.iutvalence.m3tplayer;
 
-public class Library {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Library {
+	
+	/**
+	 * The list of the imported medias.
+	 * Medias are linked with their unique ID
+	 */
+	private Map<Integer, Media> listMedia;
+
+	/**
+	 * Initializes the library.
+	 * At this moment, the library is empty.
+	 */
+	public Library() {
+		this.listMedia = new HashMap<Integer, Media>();
+	} 
+	
 }
