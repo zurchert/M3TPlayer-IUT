8583b8cad8defbea145f42707b51c6dec92ee956
granadat <granadat@PC-B103-08.iut-valence.fr>
2015-05-22 12:28:19 +0200

    Updating due to the Media class changes

--- M3TPlayer/src/fr/iutvalence/m3tplayer/Music.java
+++ M3TPlayer/src/fr/iutvalence/m3tplayer/Music.java
@@ -24,24 +24,18 @@ public class Music extends Media{
 	 * The rating value is between 0 and 5.
 	 */
 	private int rating;
-	
-	/**
-	 * The music's path
-	 */
-	private File path;
+
 	
 	/**
 	 * Creates a new Music without giving artist and album.
 	 * By default the rating is set to 0
-	 * @param id     The id of the music
 	 * @param title  The title of the music
 	 * @param path   The path of the music on the hard-drive
 	 * @param lenght The lenght of the music
 	 * @see fr.iutvalence.m3tplayer.Media
 	 */
-	public Music(int id, String title, String path, double lenght) {
-		super(id, title);
-		this.path = new File(path);
+	public Music(String title, String path, double lenght) {
+		super(title, path);
 		this.album = null;
 		this.artist = null;
 		this.lenght = lenght;
@@ -51,7 +45,6 @@ public class Music extends Media{
 	/**
 	 * Creates a new Music giving all informations such as artist, album etc.
 	 * By default the rating is set to 0.
-	 * @param id     The id of the music
 	 * @param title  The title of the music
 	 * @param path   The path of the music on the hard-drive
 	 * @param lenght The lenght of the music
@@ -59,9 +52,8 @@ public class Music extends Media{
 	 * @param album  The album of the music
 	 * @see fr.iutvalence.m3tplayer.Media
 	 */
-	public Music(int id, String title, String path, long lenght, String artist, String album) {
-		super(id, title);
-		this.path = new File(path);
+	public Music(String title, String path, long lenght, String artist, String album) {
+		super(title, path);
 		this.artist = artist;
 		this.album = album;
 		this.lenght = lenght;
@@ -119,20 +111,6 @@ public class Music extends Media{
 	public double getLenght() {
 		return this.lenght;
 	}
-	
-	/**
-	 * @return The path of the music
-	 */
-	public File getPath() {
-		return path;
-	}
-
-	/**
-	 * @param path The path to set
-	 */
-	public void setPath(File path) {
-		this.path = path;
-	}
 
 	/**
 	 * Returns a String representation of the Music object.
@@ -143,9 +121,9 @@ public class Music extends Media{
 	public String toString() {
 		StringBuilder str = new StringBuilder();
 		
-		str.append("Music "      + super.id     + "\n");
+		str.append("Music \n");
 		str.append("\t Title: "  + super.title  + "\n");
-		str.append("\t Path: "   + this.path.getPath()  + "\n");
+		str.append("\t Path: "   + super.getPath()  + "\n");
 		str.append("\t Artist: " + this.artist  + "\n");
 		str.append("\t Album: "  + this.album   + "\n");
 		str.append("\t Rating: " + this.rating  + "\n");
